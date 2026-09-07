dc43dad341141898484d4188768b86629c356691
nirholas <claudescammer@outlook.com>
2026-09-07 20:26:34 +0000

    chore: regenerate from the HookForge catalogue

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -36,6 +36,7 @@ import {DutchClearingLaunchHook} from "src/hooks/DutchClearingLaunchHook.sol";
 contract DeployDutchClearingLaunch is Script {
     uint160 internal constant FLAGS = uint160(Hooks.AFTER_INITIALIZE_FLAG | Hooks.AFTER_SWAP_FLAG);
 
+
     function run() external {
         IPoolManager manager = Chains.poolManager(block.chainid);
         require(address(manager) != address(0), "no Uniswap v4 PoolManager known for this chain");
